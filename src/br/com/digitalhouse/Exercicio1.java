43a5e19638747a74a982af9b7fc27bd32b2102c9
Digital <kukhjk>
2019-04-03 22:39:23 -0300

    Hello World

---
+++ src/br/com/digitalhouse/Exercicio1.java
@@ -0,0 +1,10 @@
+package br.com.digitalhouse;
+
+public class Exercicio1 {
+    public static boolean verificarMenor(Integer a, Integer b) {
+        return a < b;
+    }
+    public static void main(String[] args) {
+        System.out.println(verificarMenor(2,5));
+    }
+}
---
+++ src/br/com/digitalhouse/Main.java
@@ -0,0 +1,31 @@
+package br.com.digitalhouse;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        Integer umNumeroA;
+        Double umNumeroB;
+        String umaCadeiaDeTexto;
+
+        umNumeroA = 10;
+        umNumeroB = 20.0;
+        umaCadeiaDeTexto = "Cadeia de texto do Tairo";
+
+        System.out.println(umNumeroA);
+        System.out.println(umNumeroB);
+        System.out.println(umaCadeiaDeTexto);
+
+        System.out.println("Soma de A + B = " + (umNumeroA + umNumeroB));
+        System.out.println("Subtração de A - B = " + (umNumeroA - umNumeroB));
+
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        umNumeroA = scanner.nextInt();
+
+        System.out.println("Recebi: " + umNumeroA);
+    }
+}
